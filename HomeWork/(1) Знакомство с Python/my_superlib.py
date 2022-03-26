4c72d42b6eeb0834e6c1552b46570b00882e04e8
Vlad <boss.gdj@mail.ru>
2022-03-27 00:01:36 +0800

    sdsd

---
+++ HomeWork/(1) Знакомство с Python/my_superlib.py
@@ -0,0 +1,16 @@
+def sh_ms():
+    print("I'm from my_superlib...")
+
+
+
+def simple():
+    k = int(input("Enter a number:"))
+    return k ** k + 8
+
+
+
+print("Hello")
+
+
+if __name__ == '__main__':
+    print("Hi!")
